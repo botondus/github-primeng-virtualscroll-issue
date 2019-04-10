682c63487569f14b9d5f162bedbedd86dbd2c9cf
botondus <botondus@gmail.com>
2019-04-10 10:46:15 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,102 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+
+  counter = 0;
+  inmemoryData: Car[];
+  virtualCars: Car[];
+  loading: boolean;
+
+  ngOnInit() {
+    this.loading = true;
+    this.inmemoryData = [
+            {"brand": "VW", "year": 2012, "color": "Orange"},
+            {"brand": "Audi", "year": 2011, "color": "Black"},
+            {"brand": "Renault", "year": 2005, "color": "Gray"},
+            {"brand": "BMW", "year": 2003, "color": "Blue"},
+            {"brand": "Mercedes", "year": 1995, "color": "Orange"},
+            {"brand": "Volvo", "year": 2005, "color": "Black"},
+            {"brand": "Honda", "year": 2012, "color": "Yellow"},
+            {"brand": "Jaguar", "year": 2013, "color": "Orange"},
+            {"brand": "Ford", "year": 2000, "color": "Black"},
+            {"brand": "Fiat", "year": 2013, "color": "Red"},
+            {"brand": "VW", "year": 2012, "color": "Orange"},
+            {"brand": "Audi", "year": 2011, "color": "Black"},
+            {"brand": "Renault", "year": 2005, "color": "Gray"},
+            {"brand": "BMW", "year": 2003, "color": "Blue"},
+            {"brand": "Mercedes", "year": 1995, "color": "Orange"},
+            {"brand": "Volvo", "year": 2005, "color": "Black"},
+            {"brand": "Honda", "year": 2012, "color": "Yellow"},
+            {"brand": "Jaguar", "year": 2013, "color": "Orange"},
+            {"brand": "Ford", "year": 2000, "color": "Black"},
+            {"brand": "Fiat", "year": 2013, "color": "Red"},
+            {"brand": "VW", "year": 2012, "color": "Orange"},
+            {"brand": "Audi", "year": 2011, "color": "Black"},
+            {"brand": "Renault", "year": 2005, "color": "Gray"},
+            {"brand": "BMW", "year": 2003, "color": "Blue"},
+            {"brand": "Mercedes", "year": 1995, "color": "Orange"},
+            {"brand": "Volvo", "year": 2005, "color": "Black"},
+            {"brand": "Honda", "year": 2012, "color": "Yellow"},
+            {"brand": "Jaguar", "year": 2013, "color": "Orange"},
+            {"brand": "Ford", "year": 2000, "color": "Black"},
+            {"brand": "Fiat", "year": 2013, "color": "Red"},
+            {"brand": "VW", "year": 2012, "color": "Orange"},
+            {"brand": "Audi", "year": 2011, "color": "Black"},
+            {"brand": "Renault", "year": 2005, "color": "Gray"},
+            {"brand": "BMW", "year": 2003, "color": "Blue"},
+            {"brand": "Mercedes", "year": 1995, "color": "Orange"},
+            {"brand": "Volvo", "year": 2005, "color": "Black"},
+            {"brand": "Honda", "year": 2012, "color": "Yellow"},
+            {"brand": "Jaguar", "year": 2013, "color": "Orange"},
+            {"brand": "Ford", "year": 2000, "color": "Black"},
+            {"brand": "Fiat", "year": 2013, "color": "Red"}
+        ];
+  }
+  
+  onClick() {
+    this.counter++;
+  }
+
+  loadDataOnScroll(event: LazyLoadEvent) {      
+        this.loading = true;   
+
+        //for demo purposes keep loading the same dataset 
+        //in a real production application, this data should come from server by building the query with LazyLoadEvent options 
+        setTimeout(() => {
+            //last chunk
+            if (event.first === 249980)
+                this.virtualCars = this.loadChunk(event.first, 20);
+            else
+                this.virtualCars = this.loadChunk(event.first, event.rows);        
+            
+            this.loading = false;  
+        }, 250);   
+    }
+
+    loadChunk(index, length): Car[] {
+        let chunk: Car[] = [];
+        for (let i = 0; i < length; i++) {
+            chunk[i] = {...this.inmemoryData[i]};
+        } 
+
+        return chunk;
+    }
+}
+
+export class Car {
+  brand: string;
+  year: number;
+  color: string;
+}
+
+export interface LazyLoadEvent {
+    first?: number;
+    rows?: number;
+    sortField?: string;
+    sortOrder?: number;
+    globalFilter?: any;
+}
